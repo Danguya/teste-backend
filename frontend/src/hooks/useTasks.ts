79b7355aabd57850b10899a4a997ebbd42605d4e
Wilmy Danguya <daniel.yava16@gmail.com>
2024-04-14 20:02:33 +0100

    Delete frontend directory

--- frontend/src/hooks/useTasks.ts
+++
@@ -1,9 +0,0 @@
-import { useQuery } from "@tanstack/react-query";
-import { API } from "../services/axios";
-
-export function useTasks() {
-  return useQuery({queryKey:["tasks"],  queryFn: async () => {
-    const response = await API.get(`/tasks`)
-    return response.data
-  }});
-}
--- frontend/src/services/axios.ts
+++
@@ -1,8 +0,0 @@
-import axios from "axios";
-
-const BASE_URL = "http://localhost:3001/api/v1";
-
-export const API = axios.create({
-  baseURL: BASE_URL,
-});
-
